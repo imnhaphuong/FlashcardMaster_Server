cc72b49daaba88cd3130ae537b574c2b89a746f6
LinhTrang <trangnguyen24201@gmail.com>
2022-11-07 14:13:23 +0700

    update Unit

--- models/Unit.js
+++ models/Unit.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose')
 
 const UnitSchema = new mongoose.Schema({
-  unit_name: {
+  unitName: {
     type: String,
     trim: true,
     required: [
       true,
       "Topic name not provided. Cannot create topic without name",
     ],
+  },
+  creator: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
   },
   fullname: String,
   mode: Boolean,
