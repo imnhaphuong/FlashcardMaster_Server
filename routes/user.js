5a65004cb4550d4450e4f6284503f1dd2c3bdf74
LinhTrang <trangnguyen24201@gmail.com>
2022-11-27 18:27:47 +0700

    git merge main

--- routes/user.js
+++ routes/user.js
@@ -18,29 +18,5 @@ router.get("/search/:keyword", (req, res) => {
 });
 router.get("/", userController.getAllUser);
 
-// router.post("/delete", (req, res) => {
-//   Unit.findByIdAndRemove(req.body.id)
-//     .then((data) => {
-//       res.send("deleted");
-//       console.log(data);
-//     })
-//     .catch((err) => {
-//       console.log("err", err);
-//     });
-// });
-
-// router.post("/update", (req, res) => {
-//   Unit.findByIdAndUpdate(req.body.id, {
-//     unit_name: req.body.unit_name,
-//     username: req.body.username,
-//   })
-//     .then((data) => {
-//       res.send("updated");
-//       console.log(data);
-//     })
-//     .catch((err) => {
-//       console.log("err", err);
-//     });
-// });
 
 module.exports = router;
